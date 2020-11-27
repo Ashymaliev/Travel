a77d37b4fc3329b26cd6c073349e2a8072606598
Eraly33 <eraly771@gmail.com>
2020-11-27 14:11:11 +0600

    add reviews slick eraly

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// $(".reviews__slider").slick({
+//   type: "fade",
+//    rewind: true,
+// });
+// $(".reviews__slider").slick();
+
+
+
+
+
+// $("#reviews__slider", {
+//   type: "fade",
+//   rewind: true,
+// }).mount();
+
+$(".reviews__slider").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+});
